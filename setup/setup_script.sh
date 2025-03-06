c41bdfe6bd66773f5dfb0a88b30d6ff20ef8e4cc
Zeta Zen <ds@zzv.io>
2025-03-06 01:35:24 -0600

    sudo fix

--- setup/setup_script.sh
+++ setup/setup_script.sh
@@ -93,7 +93,7 @@ install_core_dependencies() {
     # Configure apt to automatically select the package maintainer's version for config files
     export DEBIAN_FRONTEND=noninteractive
     sudo apt-get -y -o Dpkg::Options::="--force-confnew"
-    install docker-ce containerd.io nginx ufw sslh
+    sudo apt-get install -y docker-ce containerd.io nginx ufw sslh
     check_status "Installing Docker and other packages"
 
     # Set up Docker Compose
